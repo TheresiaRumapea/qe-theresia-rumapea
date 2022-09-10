1dc74b4437a8172f7bd86415e4301ae6f7b79390
TheresiaRumapea <mgatheresia@gmail.com>
2022-09-10 22:05:16 +0700

    Sumbit Praktikum Section 11

---
+++ 11_Introduction Basic Programming/Praktikum/A_Problem01_LuasSegitiga.java
@@ -0,0 +1,13 @@
+public class A_Problem01_LuasSegitiga {
+    public static void main(String[] args) {
+        //input
+        float alas = 20;
+        float tinggi = 25;
+
+        //kode disini
+        float luasSegitiga = alas * tinggi/2;
+
+        System.out.println("Input: alas = " + alas + ", tinggi = " + tinggi);
+        System.out.println("Output: " + luasSegitiga);
+    }
+}
---
+++ 11_Introduction Basic Programming/Praktikum/A_Problem03_FaktorBilangan.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class A_Problem03_FaktorBilangan {
+    public static void main(String[] args) {
+        int bilangan;
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Input: ");
+        bilangan = keyboard.nextInt();
+        System.out.println("Output: ");
+
+        for(int i=1; i<= bilangan; i++){
+            if(bilangan%i == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
